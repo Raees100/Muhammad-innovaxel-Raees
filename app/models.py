73a51b682cf10cabe71de6677bcfa0e960cf7e93
Muhammad Raees <muhammadraees798@gmail.com>
2025-07-17 23:08:51 +0500

    Define URL model with short_code and click tracking

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class URL(db.Model):
+    __tablename__ = 'urls'
+
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    short_code = db.Column(db.String(10), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    clicks = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return f'<URL {self.short_code}>'
