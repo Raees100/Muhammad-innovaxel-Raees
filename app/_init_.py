d653b96388604a1e48c9b4e5bfd50f24605ab49a
Muhammad Raees <muhammadraees798@gmail.com>
2025-07-17 23:14:03 +0500

    Initialize Flask app, configure DB, and register routes

--- app/_init_.py
+++ app/_init_.py
@@ -1,15 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from .models import db
+from .routes import url_routes
 
-db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('../config.py')
 
     db.init_app(app)
-
-    from .routes import shortener_bp
     app.register_blueprint(shortener_bp)
 
+    with app.app_context():
+        db.create_all()
+    
     return app
